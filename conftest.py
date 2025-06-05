953d573dee10dfd559c54008d9f8477c92389458
Thrylouss <shoxrux.shorustamov@gmail.com>
2025-06-05 16:18:17 +0500

    ci selenium

--- conftest.py
+++ conftest.py
@@ -6,6 +6,9 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture
 def driver_chrome():
     options = Options()
+    options.add_argument("--headless=new")
+    options.add_argument("--no-sandbox")
+    options.add_argument("--disable-dev-shm-usage")
     options.add_argument("--window-size=1920,1080")
     driver = webdriver.Chrome(options=options)
     driver.maximize_window()
