3bd56057e539f81639b8d0e8d1ea5d84a90d2b82
Thrylouss <shoxrux.shorustamov@gmail.com>
2025-06-05 16:26:42 +0500

    final ci

--- conftest.py
+++ conftest.py
@@ -1,15 +1,16 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from webdriver_manager.chrome import ChromeDriverManager
+
 
 
 @pytest.fixture
 def driver_chrome():
     options = Options()
-    options.add_argument("--headless=new")
-    options.add_argument("--no-sandbox")
-    options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--headless")
     options.add_argument("--window-size=1920,1080")
+    options.add_argument("--disable-gpu")
     driver = webdriver.Chrome(options=options)
     driver.get("https://my.proweb.uz/log-in")
 
